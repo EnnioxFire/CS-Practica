e775a98b4dd2197c19d279c006cb1a15c051d837
EnnioxFire <ennioxfire@gmail.com>
2020-10-05 11:48:05 +0200

    encriptar

--- Encriptar.java
+++ Encriptar.java
@@ -1,10 +1,16 @@
+import java.io.*;
+import javax.crypto.*;
 
 public class Encriptar {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		// TODO Auto-generated method stub
-		String password="AAAAAAAAAAAAAAAA";
+		final String key = "AAAAAAAAAAAAAAAA";
+		File inpt = new File("bungee.mp3");
+		File outpt = new File("bungee.troleado");
 		
+		//aqui se haria la encriptacion
+
 	}
 
 }
