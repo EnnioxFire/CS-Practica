d009edf4fd1e4fc25712be3ce73867b16877d409
EnnioxFire <ennioxfire@gmail.com>
2020-10-05 14:29:16 +0200

    encriptar

--- Encriptar.java
+++ Encriptar.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import javax.crypto.*;
 import java.security.*;
+
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Encriptar {
@@ -9,50 +11,37 @@ public class Encriptar {
 		
 		final String key = "AAAAAAAAAAAAAAAA";
 		File inpt = new File("bungee.mp3");
-		File outpt = new File("bungee.troleado");
+		File outpt = new File("troleado");
 		
 		//aqui se haria la encriptacion
 		try{
-		Cipher cipher = Cipher.getInstance("AES");
-		Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
-		cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
 
-		/*
-		FileInputStream inputStream = new FileInputStream(inpt);
-        byte[] inputBytes = new byte[(int) inpt.length()];
-        inputStream.read(inputBytes);
-             
-        byte[] outputBytes = cipher.doFinal(inputBytes);
-             
-        FileOutputStream outputStream = new FileOutputStream(outpt);
-        outputStream.write(outputBytes);
-			 
-        inputStream.close();
-		outputStream.close();
-		*/
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			FileInputStream inputStream = new FileInputStream(inpt);
+			byte[] inputBytes = new byte[(int) inpt.length()];
+			inputStream.read(inputBytes);
+				 
+			byte[] outputBytes = cipher.doFinal(inputBytes);
+				 
+			FileOutputStream outputStream = new FileOutputStream(outpt);
 
+			outputStream.write(cipher.getIV());
+			
+			
+			outputStream.write(outputBytes);
+				 
 
-
-
-
-		FileInputStream inputStream = new FileInputStream(inpt);
-		FileOutputStream outputStream = new FileOutputStream(outpt);
-		byte[] buffer = new byte[8192];
-		int count;
-		while ((count = inputStream.read(buffer)) > 0)
-		{
-		    byte[] output = cipher.update(buffer, 0, count);
- 		   outputStream.write(output);
-		}
-		outputStream.write(cipher.doFinal());
-		outputStream.close();
-		inputStream.close();
-
-
-
-
+			System.out.print("el iv es tiene longitud " + cipher.getIV().length + " y es:");
+			for(int i = 0;i<16;i++){
+				System.out.print(cipher.getIV()[i]);
+			}
+			inputStream.close();
+			outputStream.close();
+	
 		}catch(Exception e){
-
+			System.out.print(System.lineSeparator() + "ERROR ENCRIPTAR : " + e.getMessage());
 		}
 	}
 
